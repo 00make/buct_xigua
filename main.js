eae1e23219de3dafad1be0108c48e73cbdd339ca
Yao_Z <37908699+00make@users.noreply.github.com>
2021-02-01 00:58:31 +0800

    update

--- main.js
+++ main.js
@@ -2,17 +2,13 @@
 if (false) {
     BK.Script.loadlib('GameRes://libs/qqplay-adapter.js');
 }
-
 var loadingBool = true;
 var loadingNum = 0;
-window.boot = function() {
-
+window.boot = function () {
     var settings = window._CCSettings;
     window._CCSettings = undefined;
-
     if (!settings.debug) {
         var uuids = settings.uuids;
-
         var rawAssets = settings.rawAssets;
         var assetTypes = settings.assetTypes;
         var realRawAssets = settings.rawAssets = {};
@@ -30,7 +26,6 @@ window.boot = function() {
                 realEntries[uuids[id] || id] = entry;
             }
         }
-
         var scenes = settings.scenes;
         for (var i = 0; i < scenes.length; ++i) {
             var scene = scenes[i];
@@ -38,7 +33,6 @@ window.boot = function() {
                 scene.uuid = uuids[scene.uuid];
             }
         }
-
         var packedAssets = settings.packedAssets;
         for (var packId in packedAssets) {
             var packedIds = packedAssets[packId];
@@ -48,7 +42,6 @@ window.boot = function() {
                 }
             }
         }
-
         var subpackages = settings.subpackages;
         for (var subId in subpackages) {
             var uuidArray = subpackages[subId].uuids;
@@ -61,19 +54,13 @@ window.boot = function() {
             }
         }
     }
-
     function setLoadingDisplay() {
         // Loading splash scene
         var splash = document.getElementById('splash');
         // var progressBar = splash.querySelector('.progress-bar span');
-
-
-
-        cc.loader.onProgress = function(completedCount, totalCount, item) {
-
+        cc.loader.onProgress = function (completedCount, totalCount, item) {
             loadData.completedCount = completedCount;
             loadData.totalCount = totalCount;
-
             if (loadingBool) {
                 var loadintT = document.getElementById("loadingText")
             }
@@ -81,7 +68,7 @@ window.boot = function() {
             if (loadingBool && loadingNum >= 1 && totalCount > 1) {
                 if (percent.toFixed(0) >= 100) {
                     loadintT.innerHTML = 'loading......100' + '%';
-                    setTimeout(function() {
+                    setTimeout(function () {
                         loadingBool = false;
                         loadintT.remove();
                     }, 0.1 * 1000);
@@ -97,41 +84,30 @@ window.boot = function() {
             // 	   console.log("dskpi",loadingNum);
             // 	   loadintT.innerHTML = 'loading......' + parseInt(percent)  + '%';
             // 	   if(percent.toFixed(0) >= 100){
-
             // 		   loadingBool = false;
             // 		   loadintT.remove();
             // 	   }
             // }
             // loadingNum ++;
-
             // var percent = 100 * completedCount / totalCount;
             // if (progressBar) {
             // progressBar.style.width = percent.toFixed(2) + '%';
             // }
-
-
         };
         splash.style.display = 'block';
         // progressBar.style.width = '0%';
-
-        cc.director.once(cc.Director.EVENT_AFTER_SCENE_LAUNCH, function() {
+        cc.director.once(cc.Director.EVENT_AFTER_SCENE_LAUNCH, function () {
             splash.style.display = 'none';
         });
     }
-
-    var onStart = function() {
-
+    var onStart = function () {
         cc.loader.downloader._subpackages = settings.subpackages;
-
         cc.view.enableRetina(true);
         cc.view.resizeWithBrowserSize(true);
-
         if (!false && !false) {
-
             if (cc.sys.isBrowser) {
                 setLoadingDisplay();
             }
-
             if (cc.sys.isMobile) {
                 if (settings.orientation === 'landscape') {
                     cc.view.setOrientation(cc.macro.ORIENTATION_LANDSCAPE);
@@ -146,7 +122,6 @@ window.boot = function() {
                 // ].indexOf(cc.sys.browserType) < 0);
                 cc.view.enableAutoFullScreen(false);
             }
-
             // Limit downloading max concurrent task to 2,
             // more tasks simultaneously may cause performance draw back on some android system / browsers.
             // You can adjust the number based on your own test result, you have to set it before any loading process to take effect.
@@ -154,7 +129,6 @@ window.boot = function() {
                 cc.macro.DOWNLOAD_MAX_CONCURRENT = 2;
             }
         }
-
         // function loadScene(launchScene) {
         // cc.director.loadScene(launchScene, null,
         // function () {
@@ -171,16 +145,11 @@ window.boot = function() {
         // console.log('Success to load scene: ' + launchScene);
         // }
         // );
-
         // }
-
         var launchScene = settings.launchScene;
-
         // load scene
         // loadScene(launchScene);
-
         var canvas;
-
         if (cc.sys.isBrowser) {
             canvas = document.getElementById('GameCanvas');
         }
@@ -188,23 +157,15 @@ window.boot = function() {
         console.log("landscape,", launchScene);
         var MainManger = __require("MainManage");
         MainManger.init(launchScene, cc.sys.isBrowser, canvas.style.visibility);
-
-
-
-
-
-
     };
-
     // jsList
     var jsList = settings.jsList;
-
     if (false) {
         BK.Script.loadlib();
     } else {
         var bundledScript = settings.debug ? 'src/project.dev.js' : 'src/project.js';
         if (jsList) {
-            jsList = jsList.map(function(x) {
+            jsList = jsList.map(function (x) {
                 return 'src/' + x;
             });
             jsList.push(bundledScript);
@@ -212,7 +173,6 @@ window.boot = function() {
             jsList = [bundledScript];
         }
     }
-
     var option = {
         id: 'GameCanvas',
         scenes: settings.scenes,
@@ -223,7 +183,6 @@ window.boot = function() {
         groupList: settings.groupList,
         collisionMatrix: settings.collisionMatrix,
     }
-
     // init assets
     cc.AssetLibrary.init({
         libraryPath: 'res/import',
@@ -233,16 +192,13 @@ window.boot = function() {
         md5AssetsMap: settings.md5AssetsMap,
         subpackages: settings.subpackages
     });
-
     cc.game.run(option, onStart);
 };
-
 // main.js is qqplay and jsb platform entry file, so we must leave platform init code here
 if (false) {
     BK.Script.loadlib('GameRes://src/settings.js');
     BK.Script.loadlib();
     BK.Script.loadlib('GameRes://libs/qqplay-downloader.js');
-
     var ORIENTATIONS = {
         'portrait': 1,
         'landscape left': 2,
@@ -250,17 +206,14 @@ if (false) {
     };
     BK.Director.screenMode = ORIENTATIONS[window._CCSettings.orientation];
     initAdapter();
-    cc.game.once(cc.game.EVENT_ENGINE_INITED, function() {
+    cc.game.once(cc.game.EVENT_ENGINE_INITED, function () {
         initRendererAdapter();
     });
-
     qqPlayDownloader.REMOTE_SERVER_ROOT = "";
     var prevPipe = cc.loader.md5Pipe || cc.loader.assetLoader;
     cc.loader.insertPipeAfter(prevPipe, qqPlayDownloader);
-
     window.boot();
 } else if (window.jsb) {
-
     var isRuntime = (typeof loadRuntime === 'function');
     if (isRuntime) {
         require('src/settings.js');
@@ -271,7 +224,6 @@ if (false) {
         require('src/cocos2d-jsb.js');
         require('jsb-adapter/jsb-engine.js');
     }
-
     cc.macro.CLEANUP_IMAGE_CACHE = true;
     window.boot();
 }
